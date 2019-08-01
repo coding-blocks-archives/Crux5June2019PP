4f1e2da70488092b83e2a4a35c0a00adf6c01a50
Garima Chhikara <garimachhikara128@gmail.com>
2019-08-01 17:44:17 +0530

    DP

---
+++ src/L29_Aug1/DPDemos.java
@@ -0,0 +1,261 @@
+package L29_Aug1;
+
+import java.util.Arrays;
+
+/**
+ * @author Garima Chhikara
+ * @email garima.chhikara@codingblocks.com
+ * @date 01-Aug-2019
+ *
+ */
+
+public class DPDemos {
+
+	public static void main(String[] args) {
+
+		long start = System.currentTimeMillis();
+
+		int n = 500;
+		// System.out.println(fibonacciTD(n, new int[n + 1]));
+		// System.out.println(fibonacciBU(n));
+		// System.out.println(fibonaaciBUSE(n));
+
+		// System.out.println(boardPathTD(0, n, new int[n]));
+		// System.out.println(boardPathBU(n));
+		// System.out.println(boardPathBUSE(n));
+
+		// System.out.println(mazePathTD(0, 0, n, n, new int[n + 1][n + 1]));
+		// System.out.println(mazePathBU(n, n));
+		// System.out.println(mazePathBUSE(n, n));
+
+		System.out.println(LCS("abcddbhjfcjsehfeuihrfieu", "agcfddksjhcudghcuiehfu"));
+
+		long end = System.currentTimeMillis();
+		System.out.println(end - start);
+	}
+
+	// T : O(2^n) S : Rec extra space
+	public static int fibonacci(int n) {
+
+		if (n == 0 || n == 1) {
+			return n;
+		}
+
+		int fnm1 = fibonacci(n - 1);
+		int fnm2 = fibonacci(n - 2);
+
+		int fn = fnm1 + fnm2;
+
+		return fn;
+
+	}
+
+	// T : O(n) S : Rec extra space + array extra space
+	public static int fibonacciTD(int n, int[] strg) {
+
+		if (n == 0 || n == 1) {
+			return n;
+		}
+
+		if (strg[n] != 0) { // use
+			return strg[n];
+		}
+
+		int fnm1 = fibonacciTD(n - 1, strg);
+		int fnm2 = fibonacciTD(n - 2, strg);
+
+		int fn = fnm1 + fnm2;
+
+		strg[n] = fn; // store
+
+		return fn;
+
+	}
+
+	// T : O(n) S : O(n)
+	public static int fibonacciBU(int n) {
+
+		int[] strg = new int[n + 1];
+
+		strg[0] = 0;
+		strg[1] = 1;
+
+		for (int i = 2; i < strg.length; i++) {
+			strg[i] = strg[i - 1] + strg[i - 2];
+		}
+
+		return strg[n];
+	}
+
+	// T : O(n) S : O(1)
+	public static int fibonaaciBUSE(int n) {
+
+		int[] strg = new int[2];
+		strg[0] = 0;
+		strg[1] = 1;
+
+		for (int i = 1; i <= n - 1; i++) {
+			int sum = strg[0] + strg[1];
+			strg[0] = strg[1];
+			strg[1] = sum;
+
+		}
+
+		return strg[1];
+
+	}
+
+	// T : O(n) S : Rec extra space + array space
+	public static int boardPathTD(int curr, int end, int[] strg) {
+
+		if (curr == end) {
+			return 1;
+		}
+
+		if (curr > end) {
+			return 0;
+		}
+
+		if (strg[curr] != 0) {
+			return strg[curr];
+		}
+
+		int c = 0;
+		for (int dice = 1; dice <= 6; dice++) {
+			c += boardPathTD(curr + dice, end, strg);
+		}
+
+		strg[curr] = c;
+
+		return c;
+
+	}
+
+	// T : O(n) S : O(n)
+	public static int boardPathBU(int n) {
+
+		int[] strg = new int[n + 6];
+
+		strg[n] = 1;
+
+		for (int i = n - 1; i >= 0; i--) {
+			strg[i] = strg[i + 1] + strg[i + 2] + strg[i + 3] + strg[i + 4] + strg[i + 5] + strg[i + 6];
+		}
+
+		return strg[0];
+
+	}
+
+	// T : O(n) S : O(1)
+	public static int boardPathBUSE(int n) {
+
+		int[] strg = new int[6];
+
+		strg[0] = 1;
+
+		for (int slide = 1; slide <= n; slide++) {
+
+			int sum = strg[0] + strg[1] + strg[2] + strg[3] + strg[4] + strg[5];
+
+			strg[5] = strg[4];
+			strg[4] = strg[3];
+			strg[3] = strg[2];
+			strg[2] = strg[1];
+			strg[1] = strg[0];
+			strg[0] = sum;
+
+		}
+
+		return strg[0];
+
+	}
+
+	// T : O(n^2) S : Rec extra space + 2D Array
+	public static int mazePathTD(int cr, int cc, int er, int ec, int[][] strg) {
+
+		if (cr == er && cc == ec) {
+			return 1;
+		}
+
+		if (cr > er || cc > ec) {
+			return 0;
+		}
+
+		if (strg[cr][cc] != 0) {
+			return strg[cr][cc];
+		}
+
+		int ch = mazePathTD(cr, cc + 1, er, ec, strg);
+		int cv = mazePathTD(cr + 1, cc, er, ec, strg);
+
+		strg[cr][cc] = ch + cv;
+
+		return ch + cv;
+
+	}
+
+	// T : O(er * ec) S : O(er * ec)
+	public static int mazePathBU(int er, int ec) {
+
+		int[][] strg = new int[er + 2][ec + 2];
+
+		strg[er][ec] = 1;
+
+		for (int row = er; row >= 0; row--) {
+			for (int col = ec; col >= 0; col--) {
+
+				if (row == er && col == ec)
+					continue;
+
+				strg[row][col] = strg[row + 1][col] + strg[row][col + 1];
+			}
+		}
+
+		return strg[0][0];
+
+	}
+
+	// T : O(er * ec) S : O(ec)
+	public static int mazePathBUSE(int er, int ec) {
+
+		int[] strg = new int[ec + 1];
+
+		Arrays.fill(strg, 1);
+
+		for (int slide = er - 1; slide >= 0; slide--) {
+
+			for (int col = ec - 1; col >= 0; col--) {
+				strg[col] = strg[col] + strg[col + 1];
+			}
+		}
+
+		return strg[0];
+	}
+
+	public static int LCS(String s1, String s2) {
+
+		if (s1.length() == 0 || s2.length() == 0) {
+			return 0;
+		}
+
+		char ch1 = s1.charAt(0);
+		char ch2 = s2.charAt(0);
+
+		String ros1 = s1.substring(1);
+		String ros2 = s2.substring(1);
+
+		int ans = 0;
+		if (ch1 == ch2) {
+			ans = LCS(ros1, ros2) + 1;
+		} else {
+			int o1 = LCS(s1, ros2);
+			int o2 = LCS(ros1, s2);
+
+			ans = Math.max(o1, o2);
+		}
+
+		return ans;
+
+	}
+
+}
